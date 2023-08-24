1f330d1c4450ac3a1459bd97faa3c90be78ac365
Florentina <florentinaantigha27@gmail.com>
2023-08-24 13:43:40 +0100

    add new shell script

---
+++ movies.sh
@@ -0,0 +1,3 @@
+cp /mnt/c/Users/user/Desktop/Movies/Babylon\ \(2022\)\ \(NetNaija.com\).mp4 Legion\ of\ Super-Heroes\ \(2023\)\ \(NetNaija.com\).mkv Transformers\ Rise\ of\ the\ Beasts\ \(2023\)\ \(NetNaija.com\).mkv  /mnt/c/Users/user/Desktop/'Florentina Movies'
+echo Copying Completed
+pwd

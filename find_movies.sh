a0bf5d85485b5f0d367afbc70b6bb69f784bd286
Florentina <florentinaantigha27@gmail.com>
2023-08-31 05:34:00 +0100

    Created Shell Script to find movies starting with letter 'B'

---
+++ find_movies.sh
@@ -0,0 +1,4 @@
+find . -maxdepth 1 -type f -iname "b*" -exec cp {} /mnt/c/Users/user/Desktop/Movies_starting_with_B \;
+echo Task on learning about the 'find' command and its arguments, as well as copying files from the 'find' command and getting familiar with GitHub, has been Completed.
+echo Unto the next step.
+pwd
